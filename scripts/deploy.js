5d0d8358b2dda7fbc1a66515bc6ccea31c46f5c0
Swati Arya <144891393+arya-s-cmd@users.noreply.github.com>
2025-06-12 20:32:10 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,6 +4,7 @@ async function main() {
   const Tracking = await hre.ethers.getContractFactory("Tracking");
   const tracking = await Tracking.deploy();
 
+  
   await tracking.deployed(); 
 
   console.log(`Tracking deployed to: ${tracking.address}`);
